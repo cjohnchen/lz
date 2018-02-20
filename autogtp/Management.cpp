0351a4b17cca9bdfd03b11202cdb074a732bea73
Junyan Xu <junyanxumath@gmail.com>
2018-02-20 00:04:45 -0500

    Update Management.cpp

--- autogtp/Management.cpp
+++ autogtp/Management.cpp
@@ -77,7 +77,6 @@ void Management::runTuningProcess(const QString &tuneCmdLine) {
 
 Order Management::getWork(const QFileInfo &file) {
     Order o;
-<<<<<<< HEAD
     if(!file.fileName().isEmpty()) {
         QTextStream(stdout) << "Got previously stored file" <<endl;
         o.load(file.fileName());
@@ -89,14 +88,6 @@ Order Management::getWork(const QFileInfo &file) {
     } else {
         return getWork();
     }
-=======
-    o.load(file.fileName());
-    QFile::remove(file.fileName());
-    m_lockFile->unlock();
-    delete m_lockFile;
-    m_lockFile = nullptr;
-    return o;
->>>>>>> remotes/origin/patch-6
 }
 
 void Management::giveAssignments() {
