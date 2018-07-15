0c29900c7b96db489c157cff4a699bcf56da8407
Junyan Xu <xu56@indiana.edu>
2018-07-15 02:45:34 -0400

    prior probabilities

--- src/UCTSearch.cpp
+++ src/UCTSearch.cpp
@@ -43,12 +43,12 @@ constexpr int UCTSearch::UNLIMITED_PLAYOUTS;
 
 class OutputAnalysisData {
 public:
-    OutputAnalysisData(const std::string& move, int visits, int winrate, std::string pv) :
-        m_move(move), m_visits(visits), m_winrate(winrate), m_pv(pv) {};
+    OutputAnalysisData(const std::string& move, int visits, int winrate, std::string N_num, std::string pv) :
+        m_move(move), m_visits(visits), m_winrate(winrate), m_N_num(N_num), m_pv(pv) {};
 
     std::string get_info_string(int order) const {
         auto tmp = "info move " + m_move + " visits " + std::to_string(m_visits) +
-                          " winrate " + std::to_string(m_winrate);
+            " winrate " + std::to_string(m_winrate) + " N " + m_N_num;
         if (order >= 0) {
             tmp += " order " + std::to_string(order);
         }
@@ -67,9 +67,9 @@ private:
     std::string m_move;
     int m_visits;
     int m_winrate;
+    std::string m_N_num;
     std::string m_pv;
 };
-
 
 UCTSearch::UCTSearch(GameState& g)
     : m_rootstate(g) {
